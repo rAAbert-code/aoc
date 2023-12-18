da44fffeb31934e24f1880009382910abbb899a0
rAAbert-code <rAAbert.github@gmail.com>
2023-12-18 22:37:22 +0100

    2023/1-1

---
+++ 2023/1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+#jdfjkdsf
+
+import os
+import sys
+import re
+import copy
+
+def getfilename():
+    args = sys.argv
+    dir_name = os.path.dirname(args[0])
+    file_name = os.path.basename(args[0])[0]
+    if len(args) < 2:
+        file_name += ".data"
+    elif args[1] == "-s":
+        file_name += ".sample"
+    else:
+        sys.exit("Unknown argument")
+    return dir_name + "/input/" + file_name
+
+def puzzle(file):
+    sum = 0
+
+    for line in file:
+        first = last = -1
+        for index, char in enumerate(line):
+            if char.isdigit():
+                if first == -1:
+                    first = int(char) 
+                last = int(char)
+        number = first*10 + last
+        print(line,number)
+        sum += number
+    print("sum:", sum)
+
+def main():
+    file_name = getfilename()
+
+    with open(file_name, "r") as file:
+        puzzle(file)
+
+if __name__ == "__main__":
+    main()
+
