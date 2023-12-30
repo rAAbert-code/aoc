50c7eb53c84540f24508f8a8619d2e1fa37e468e
rAAbert-code <rAAbert.github@gmail.com>
2023-12-30 19:54:47 +0100

    Updated tempalte.py

--- template.py
+++ template.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
-#jdfjkdsf
 
 import os
 import sys
 import re
 import copy
 
-def getfilename():
-    args = sys.argv
-    dir_name = os.path.dirname(args[0])
-    file_name = os.path.basename(args[0])[0]
-    if len(args) < 2:
-        file_name += ".data"
-    elif args[1] == "-s":
-        file_name += ".sample"
-    else:
-        sys.exit("Unknown argument")
-    return dir_name + "/input/" + file_name
+def p1(lines):
+    return 0
 
-def puzzle(file):
-    pass
-
-def main():
-    file_name = getfilename()
-
-    with open(file_name, "r") as file:
-        puzzle(file)
-
-if __name__ == "__main__":
-    main()
+def p2(lines):
+    return 0
 
